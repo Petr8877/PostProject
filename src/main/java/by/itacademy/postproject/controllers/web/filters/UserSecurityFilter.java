734cd4642874a6e674e8f6710a59d605c457c3ab
Aleksey <lexaapanas@mail.ru>
2022-12-17 19:53:24 +0300

    add Filter

---
+++ src/main/java/by/itacademy/postproject/controllers/web/filters/UserSecurityFilter.java
@@ -0,0 +1,35 @@
+package by.itacademy.postproject.controllers.web.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/api/message*"})
+public class UserSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String contextPath = req.getContextPath();
+        HttpSession session = req.getSession();
+        if ((session != null) && (session.getAttribute("user") != null)) {
+            chain.doFilter(request, response);
+        } else {
+            res.sendRedirect(contextPath + "/api/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
