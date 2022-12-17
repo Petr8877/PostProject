966c8d4e298567f0edebed74b7138d0c174bef3a
Aleksey <lexaapanas@mail.ru>
2022-12-17 20:01:04 +0300

    changed Filter

--- src/main/java/by/itacademy/postproject/controllers/web/filters/UserSecurityFilter.java
+++ src/main/java/by/itacademy/postproject/controllers/web/filters/UserSecurityFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/api/message*"})
+@WebFilter(urlPatterns = {"/api/message"})
 public class UserSecurityFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
