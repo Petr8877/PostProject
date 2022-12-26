f6273cc9fc72d4bab815aa0853a3f00f67106be5
Petr8877 <kasperovichpetr@gmail.com>
2022-12-27 02:31:29 +0300

    Обновленные jsp

--- src/main/java/by/itacademy/postproject/controllers/web/servlets/servlrtsJSP/MailPage.java
+++ src/main/java/by/itacademy/postproject/controllers/web/servlets/servlrtsJSP/MailPage.java
@@ -1,5 +1,8 @@
 package by.itacademy.postproject.controllers.web.servlets.servlrtsJSP;
 
+import by.itacademy.postproject.dto.UserDTO;
+import by.itacademy.postproject.dto.UserSessionDTO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +15,8 @@ public class MailPage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserSessionDTO dto = (UserSessionDTO) req.getSession().getAttribute("user");
+        req.setAttribute("name", dto.getLogin());
         req.getRequestDispatcher("/views/Mail.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/views/Include.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java"
+        contentType="text/html; charset=UTF-8"
+        pageEncoding="UTF-8"%>
+<html>
+<head>
+<title>И снова зраствуйте</title>
+<style>
+.logo {
+color: Azure;
+font-family: Times New Roman;
+font-size: 18px;
+}
+</style>
+</head>
+<body>
+<div style="text-align:center;">
+<a href="${pageContext.request.contextPath}/ui/" class="logo">ПОЧТОВИК</a><br><br>
+</div>
+</body>
+</html>
